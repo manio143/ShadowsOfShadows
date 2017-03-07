d9731552de037756bfbbaa5303c4095e51232984
manio143 <marian.dziubiak@gmail.com>
2017-03-07 11:07:59 +0100

    Add project files

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿using SConsole = SadConsole.Consoles.Console;
+using SadConsole;
+
+namespace ShadowsOfShadows
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Engine.Initialize ("C64.font", 80, 25);
+			Engine.EngineStart += (sender, e) => {
+				var console = (SConsole)Engine.ActiveConsole;
+				console.Print (1, 1, "Hello World!");
+			};
+			Engine.Run ();
+		}
+	}
+}
