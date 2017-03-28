a4b076477d6238b2830a1710c7fdb84553f90b3e
manio143 <marian.dziubiak@gmail.com>
2017-03-28 10:54:43 +0200

    Parameterize game window size

--- src/Program.cs
+++ src/Program.cs
@@ -7,16 +7,19 @@ namespace ShadowsOfShadows
 {
 	class MainClass
 	{
+		const int WIDTH = 80;
+		const int HEIGHT = 40;
+
 		public static void Main (string[] args)
 		{
-			SadConsole.Game.Create("C64.font", 80, 25);
+			SadConsole.Game.Create("C64.font", WIDTH, HEIGHT);
 			SadConsole.Game.OnInitialize = Initialize;
 			SadConsole.Game.Instance.Run();
 		}
 
 		public static void Initialize()
 		{
-			Global.CurrentScreen = new Screen (80, 25);
+			Global.CurrentScreen = new Screen (WIDTH, HEIGHT);
 		}
 	}
 }
