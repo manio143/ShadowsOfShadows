34f647ca5515a0d95249cf7e6c37f70fb2ca1643
Marian Dziubiak <marian.dziubiak@gmail.com>
2017-05-08 19:26:58 +0200

    Add glyph constructor to ConsoleRenderable

--- src/Renderables/ConsoleRenderable.cs
+++ src/Renderables/ConsoleRenderable.cs
@@ -7,11 +7,15 @@ namespace ShadowsOfShadows.Renderables
 {
 	public class ConsoleRenderable : IRenderable
 	{
-		public ConsoleRenderable ( char symbol)
+		public ConsoleRenderable (char symbol)
 		{
 			ConsoleObject = ConsoleObjects.CreateFromChar(symbol);
 		}
 
+	    public ConsoleRenderable(int glyph)
+	    {
+	        ConsoleObject = ConsoleObjects.CreateFromGlyph(glyph);
+	    }
 
 		public GameObject ConsoleObject { get;} 
 	}
