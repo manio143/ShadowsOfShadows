a35260cfd1606eff144e709d43fa88cac1998da4
Marian Dziubiak <marian.dziubiak@gmail.com>
2017-05-15 18:42:26 +0200

    Add coloring possibility to ConsoleRenderable
    Resolves #23

--- src/Renderables/ConsoleRenderable.cs
+++ src/Renderables/ConsoleRenderable.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Microsoft.Xna.Framework;
 using SadConsole.GameHelpers;
 using ShadowsOfShadows.Helpers;
 
@@ -7,14 +7,14 @@ namespace ShadowsOfShadows.Renderables
 {
 	public class ConsoleRenderable : IRenderable
 	{
-		public ConsoleRenderable (char symbol)
+		public ConsoleRenderable (char symbol, Color color = new Color())
 		{
-			ConsoleObject = ConsoleObjects.CreateFromChar(symbol);
+			ConsoleObject = ConsoleObjects.CreateFromChar(symbol, color);
 		}
 
-	    public ConsoleRenderable(int glyph)
+	    public ConsoleRenderable(int glyph, Color color = new Color())
 	    {
-	        ConsoleObject = ConsoleObjects.CreateFromGlyph(glyph);
+	        ConsoleObject = ConsoleObjects.CreateFromGlyph(glyph, color);
 	    }
 
 		public GameObject ConsoleObject { get;} 
